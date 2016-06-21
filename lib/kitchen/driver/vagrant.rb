6d0c89fe6086b6d830ccb86ab93d5fdf32c2f086
Todd Trimmer <todd.trimmer@gmail.com>
2016-06-21 15:58:26 -0500

    typo and gnarled unless logic

--- lib/kitchen/driver/vagrant.rb
+++ lib/kitchen/driver/vagrant.rb
@@ -283,8 +283,8 @@ module Kitchen
       # @raise [ActionFailed] if the Vagrantfile template was not found
       # @api private
       def render_template
-        unless config[:box_download_ca_cert].nil? config[:box_download_ca_cert] = File.expand_path(
-          config[:box_download_ca_cert, config[:kitchen_root]])
+        config[:box_download_ca_cert] = File.expand_path(
+          config[:box_download_ca_cert], config[:kitchen_root]) unless config[:box_download_ca_cert].nil?
         template = File.expand_path(
           config[:vagrantfile_erb], config[:kitchen_root])
 
