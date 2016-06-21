9bddc665f400667a753126543ee34d0328194b36
Todd Trimmer <todd.trimmer@gmail.com>
2016-06-21 15:33:30 -0500

    adding support for box_download_ca_cert

--- lib/kitchen/driver/vagrant.rb
+++ lib/kitchen/driver/vagrant.rb
@@ -45,6 +45,8 @@ module Kitchen
 
       default_config :box_download_insecure, nil
 
+      default_config :box_download_ca_cert, nil
+
       default_config(:box_url) { |driver| driver.default_box_url }
 
       default_config :box_version, nil
@@ -281,6 +283,8 @@ module Kitchen
       # @raise [ActionFailed] if the Vagrantfile template was not found
       # @api private
       def render_template
+        unless config[:box_download_ca_cert].nil? config[:box_download_ca_cert] = File.expand_path(
+          config[:box_download_ca_cert, config[:kitchen_root]])
         template = File.expand_path(
           config[:vagrantfile_erb], config[:kitchen_root])
 
--- lib/kitchen/driver/vagrant_version.rb
+++ lib/kitchen/driver/vagrant_version.rb
@@ -21,6 +21,6 @@ module Kitchen
   module Driver
 
     # Version string for Vagrant Kitchen driver
-    VAGRANT_VERSION = "0.20.0"
+    VAGRANT_VERSION = "0.20.1"
   end
 end
