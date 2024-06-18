da5aa3a21b73cb75e5594154c89f166bab24268b
Corey Hemminger <hemminger@hotmail.com>
2024-06-18 18:09:09 -0500

    fix: add release please configs
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

---
+++ test/integration/default/default_test.rb
@@ -0,0 +1,9 @@
+# Chef InSpec test for kitchen-vagrant
+
+# The Chef InSpec reference, with examples and extensive documentation, can be
+# found at https://docs.chef.io/inspec/resources/
+
+user_name = os.windows? ? 'Administrator' : 'root'
+describe user(user_name) do
+  it { should exist }
+end
